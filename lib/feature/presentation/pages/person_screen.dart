d4dbc6d6ece15f479f6d303e2909d1266d68e906
taras3189 <taras3189@gmail.com>
2023-01-11 17:52:11 +0200

    PersonCard commit

--- lib/feature/presentation/pages/person_screen.dart
+++ lib/feature/presentation/pages/person_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty/feature/presentation/widgets/custom_search_delegate.dart';
+import 'package:rick_and_morty/feature/presentation/widgets/persons_list_widget.dart';
+
+
 
 
 class HomePage extends StatelessWidget {
@@ -6,6 +10,21 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Characters'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            color: Colors.white,
+            onPressed: () {
+              showSearch(context: context, delegate: CustomSearchDelegate());
+            },
+          )
+        ],
+      ),
+      body: PersonsList(),
+    );
   }
 }
